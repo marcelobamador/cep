cf6c571b694908c0ba877307757ed1cd55998fd1
marcelo <marcelo.bruno19@hotmail.com>
2025-06-24 14:37:15 -0300

    feat: external URL configuration + preparation for external config / properties binding,

--- src/main/kotlin/br/com/cep/ZipCodeApplication.kt
+++ src/main/kotlin/br/com/cep/ZipCodeApplication.kt
@@ -1,10 +1,13 @@
 package br.com.cep
 
+import br.com.cep.config.ViaZipProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+@EnableConfigurationProperties(ViaZipProperties::class)
 open class ZipCodeApplication
 
 fun main(args: Array<String>) {
